763c760cfd05b6be15c7b221ee7fc7c3f8e2e695
Jerry Velazquez <jvr572@gmail.com>
2019-02-06 16:14:20 +0000

    Automatically backed up by Learn

---
+++ learn.js
@@ -0,0 +1,23 @@
+/*function sayHelloTo(firstName) {
+  console.log(`Hello, ${firstName}!`)
+}
+ 
+sayHelloTo('Isabel') 
+sayHelloTo("Jane") 
+sayHelloTo("R2-D2") 
+sayHelloTo(1)*/
+
+
+function say(greeting, firstName) {
+  console.log('I was called!')
+  return '${greeting}, ${firstName}!'
+}
+say('Hello", "Sofia')
+
+
+/*function add(x, y) {
+  return x + y
+}
+
+add(1, 2)
+add(80, 9000)*/
